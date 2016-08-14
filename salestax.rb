6097fca394bc6b8ab63541fcecb7eadd4333b238
ChrisP <600steps@gmail.com>
2016-08-13 22:31:48 -0400

    working version with one shopping cart-with sub class

--- salestax.rb
+++ salestax.rb
@@ -1,6 +1,6 @@
 class Item
   attr_reader :name
-  attr_accessor :quant, :price, :sales_tax, :import_tax
+  attr_accessor :quant, :price, :sales_tax, :import_tax, :total_tax, :total_item_cost
 
   def initialize (quant, name, price, sales_tax, import_tax)
     @quant = quant
@@ -8,47 +8,59 @@ class Item
     @price = price
     @sales_tax = sales_tax
     @import_tax = import_tax
+    @total_tax = total_tax
+    @total_item_cost = 0
+
   end
 
   def tax
+      #@@sales_tax_total = 0
       sales_tax_rate = 0.10
       import_tax_rate = 0.05
 
       if @sales_tax == true && @import_tax == true
-        @price * (sales_tax_rate + import_tax_rate)
+        @total_tax = @price * (sales_tax_rate + import_tax_rate)
       elsif @sales_tax == true && @import_tax == false
-        @price * sales_tax_rate
+        @total_tax = @price * sales_tax_rate
       elsif @sales_tax == false && @import_tax == true
-        @price * import_tax_rate
+        @total_tax = @price * import_tax_rate
       else @sales_tax == false && @import_tax == false
-        @price
+        @total_tax = 0
       end
+    #  @@sales_tax_total+=@total_tax
   end
 
-  def self.total_item_cost
-    @price + tax
+  def total_item_cost
+    @total_item_cost = @price + tax
   end
 
 end # ends Item class
 
-class Receipt
+class Receipt < Item
   attr_accessor :basket
+  attr_accessor :total_tax, :total
 
   def initialize
     @basket = []
+    @@sales_tax_total = 0
+    @@basket_total_sale = 0
+
   end
 
   def add_to_cart(item)
-    @basket.push(item)
-    puts "test basket"
+    @basket << item
+    @@sales_tax_total += item.tax
+    @@basket_total_sale += item.total_item_cost
+
   end
 
   def start
     @basket.each do |x|
-    puts "#{x.quant} #{x.name}: #{Item.total_item_cost}"
-    puts "Sales Tax: "
-    puts "Total:"
+    puts "#{x.quant} #{x.name}: #{x.total_item_cost.round(2)}"
   end
+  puts "Sales Tax: #{@@sales_tax_total.round(2)}"
+  puts "Total: #{@@basket_total_sale.round(2)}"
+
 
 end # end of Receipt class
 
